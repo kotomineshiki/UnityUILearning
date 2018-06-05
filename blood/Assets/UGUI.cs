d07a337df37f75b0a1bc1ced7af270393d153048
yiqu zhu <kotomineshiki@outlook.com>
2018-06-05 22:16:13 +0800

    final

---
+++ blood/Assets/UGUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UGUI : MonoBehaviour {
+    public RectTransform bloodcanvas;
+    public Slider Enemyblood;
+    public Camera theCamera;
+    public float resultBlood = 0f;
+    void Start () {
+        resultBlood = Enemyblood.value;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bloodcanvas.rotation = theCamera.transform.rotation;
+	}
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(20, 30, 80, 20), "敌人加血"))
+        {
+            
+            resultBlood += 0.1f;
+        }
+        if (GUI.Button(new Rect(150, 30, 80, 20), "敌人减血"))
+        {
+            resultBlood -= 0.1f;
+        }
+        Enemyblood.value = Mathf.Lerp(Enemyblood.value, resultBlood, 0.05f);
+    }
+}
---
+++ blood/Assets/UserInterface.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInterface : MonoBehaviour {
+    public float bloodValue = 0.0f;
+    private float ResultValue;
+    private Rect rctBloodBar;
+    private Rect rctUpButton;
+    private Rect rctDownButton;
+    private bool onoff;
+
+    // Use this for initialization
+    void Start () {
+        rctBloodBar = new Rect(20, 20, 200, 20);
+        //加血-按钮  
+        rctUpButton = new Rect(20, 50, 80, 20);
+        //减血-按钮  
+        rctDownButton = new Rect(150, 50, 80, 20);
+        ResultValue = bloodValue;
+
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        if (GUI.Button(rctUpButton, "我方加血"))
+        {
+            ResultValue += 0.1f;
+        }
+        if (GUI.Button(rctDownButton, "我方减血"))
+        {
+            ResultValue -= 0.1f;
+        }
+        if (ResultValue > 1.0f)
+        {
+            ResultValue = 1.0f;
+        }
+        if (ResultValue < 0.0f)
+        {
+            ResultValue = 0.0f;
+        }
+        //插值计算HP值
+
+        bloodValue = Mathf.Lerp(bloodValue, ResultValue, 0.05f);
+
+        GUI.HorizontalScrollbar(rctBloodBar, 0.0f, bloodValue, 0.0f, 1.0f);
+    }
+
+}
